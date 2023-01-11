93d80f44b9406677183248d2580d4475a4d7c319
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 12:41:53 -0300

    Inserindo exercício 13

---
+++ Exercício 13/Exercício 13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Faça uma classe SmartWatch com a contagem dos passos diários de uma pessoa (pedômetro). Garanta utilizando
+//acessors (get/set) que os passos apenas podem ser incrementados e um método resetOnMidnight() 
+//que reseta os passos para 0.
+
+using Exercício_13;
+
+SmartWatch sm1 = new SmartWatch();
+sm1.IncrementaPassos(40);
+sm1.IncrementaPassos(-20);
+sm1.IncrementaPassos(2);
+Console.WriteLine($"Passos dados: {sm1.Passos}");
+sm1.ResetOnMidnight();
+sm1.IncrementaPassos(4);
+Console.WriteLine($"Passos dados: {sm1.Passos}");
---
+++ Exercício 13/Exercício 13/SmartWatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_13
+{
+    internal class SmartWatch
+    {
+        public int Passos { get; private set; }
+        public SmartWatch()
+        {
+            Passos = 0;
+        }
+        public void IncrementaPassos(int passosDados)
+        {
+            if (passosDados < 0)
+            {
+                passosDados *= -1;
+            }
+            Passos += passosDados;
+        }
+        public void ResetOnMidnight()
+        {
+            Passos = 0;
+        }
+    }
+}
