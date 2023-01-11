bb52c30bf006e25feb8abfcebd687a8829918a85
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 11:10:24 -0300

    inserindo exercício 9

---
+++ Exercício 9/Exercício 9/Program.cs
@@ -0,0 +1,34 @@
+﻿//Crie uma classe Time que tenha como propriedade o nome do país, nome do esporte e a pontuação dele em um torneio.
+//Sua pontuação pode ser acrescida por um método pontua que recebe como parâmetro "W" para vitória (+3 pontos),
+//"L" para derrota(+0 pontos) e "D" para empate(+1 ponto).
+
+using Exercício_9;
+Time t1 = new Time("Brasil", "Futebol");
+Console.WriteLine($"digite quantas partidas de {t1.Esporte} a seleção {t1.Pais} jogou no ano");
+Random rd = new Random();
+int partida;
+char resultado;
+int.TryParse(Console.ReadLine(), out int partidas);
+Console.WriteLine();
+for (int i = 0; i < partidas; i++)
+{
+    partida = rd.Next(1,4);
+    if (partida == 1)
+    {
+        resultado = 'W';
+        Console.WriteLine($"Você ganhou a {i+1}ª partida");
+    }
+    else if (partida == 2)
+    {
+        resultado = 'D';
+        Console.WriteLine($"Você empatou a {i + 1}ª partida");
+    }
+    else
+    {
+        resultado = 'L';
+        Console.WriteLine($"Você perdeu a {i + 1}ª partida");
+    }
+    t1.Pontuar(resultado);
+
+}
+Console.WriteLine($"\nA seleção {t1.Pais} no esporte {t1.Esporte} jogou {partidas} partidas no ano! e ficou com {t1.Pontuação} pontos");
---
+++ Exercício 9/Exercício 9/Time.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_9
+{
+    internal class Time
+    {
+        public string Pais { get; set; }
+        public string Esporte { get; set; }
+        public int Pontuação { get; set; }
+
+        public Time(string pais, string esporte)
+        {
+            Pais = pais;
+            Esporte = esporte;
+            Pontuação = 0;
+        }   
+        public void Pontuar(char resultado)
+        {
+            if(resultado == 'W')
+            {
+                Pontuação += 3;
+            }
+            else if(resultado == 'D')
+            {
+                Pontuação ++;
+            }
+            else if(resultado == 'L')
+            {
+                Pontuação += 0;
+            }
+        }
+    }
+}
