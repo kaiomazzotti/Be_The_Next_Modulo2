f1d2f0ffe11dc2793630e78c019b3fa8c843e668
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 12:26:26 -0300

    Inserindo exercício 12

---
+++ Exercício 12/Exercício 12/Perfil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_12
+{
+    internal class Perfil
+    {
+        public string Nome { get; set; }
+        public string FotoDePerfil { get; private set; }
+        public string Tema { get; private set; }
+        public Perfil(string nome)
+        {
+            this.Nome = nome;
+        }
+        public void TrocaFotoPerfil()
+        { bool verif;
+            string url = "";
+            do
+            {
+                Console.WriteLine("insira o URL da sua foto: ");
+                url = Console.ReadLine();
+                if (url.StartsWith("http") || url.StartsWith("https"))
+                {
+                    this.FotoDePerfil = url;
+                    verif = true;
+                }
+                else
+                {
+                    Console.WriteLine("insira um URL válido (inicie com http ou https)");
+                    verif = false;
+                }
+            } while (verif == false);
+        }
+        public void MudaTema()
+        {
+            string tema = "";
+            bool verif;
+            do
+            {
+                Console.WriteLine("Digite qual tema você deseja! (light ou dark) ");
+                tema = Console.ReadLine();
+                if (tema.ToLower() == "light" || tema.ToLower() == "dark")
+                {
+                    verif = true;
+                    this.Tema = tema;
+                }
+                else
+                {
+                    verif = false;
+                    Console.WriteLine("insira um tema existente!");
+                }
+            }
+            while (verif == false);
+        }
+    }
+}
---
+++ Exercício 12/Exercício 12/Program.cs
@@ -0,0 +1,11 @@
+﻿//Faça uma classe Perfil que tem as seguintes informações: nome, avatar(foto de perfil) e tema.
+//Utilize acessors (get/set) para garantir que o tema só pode ser "light" ou "dark" e que a foto de perfil 
+//deve ser uma url (iniciada com http ou https).
+
+using Exercício_12;
+
+Perfil p1 = new Perfil("Kaio Mazzotti");
+p1.TrocaFotoPerfil();
+p1.MudaTema();
+
+Console.WriteLine($"Usuário = {p1.Nome} \nURL foto de Perfil  = [{p1.FotoDePerfil}]\nTema = [{p1.Tema.ToUpper()}]");
