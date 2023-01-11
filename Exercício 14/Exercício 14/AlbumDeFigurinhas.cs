be9a52df24b7347c5b0357e120b7555cd582e889
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 13:47:59 -0300

    Inserindo exercício 14

---
+++ Exercício 14/Exercício 14/AlbumDeFigurinhas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_14
+{
+    internal class AlbumDeFigurinhas
+    {
+        public int[] QuantidadeFigurinhasAlbum { get; set; }
+        public int[] FigurinhasColadas { get; private set; }
+        public AlbumDeFigurinhas(int quantidadeFigurinhas)
+        {
+
+            QuantidadeFigurinhasAlbum = new int[quantidadeFigurinhas];
+            for (int i = 0; i < quantidadeFigurinhas; i++)
+            {
+                QuantidadeFigurinhasAlbum[i] = i + 1; 
+            }
+            FigurinhasColadas = new int[quantidadeFigurinhas];
+        }
+
+        public void ColarFigurinhas(int figurinha)
+        {
+            if (QuantidadeFigurinhasAlbum.Length < figurinha )
+            {
+                Console.WriteLine("Numero da figurinha não existe nesse álbum");
+            }
+            else if (QuantidadeFigurinhasAlbum[figurinha - 1] == FigurinhasColadas[figurinha - 1])
+            {
+                Console.WriteLine("Figurinha já existe no album");
+            }
+            else
+            {
+                FigurinhasColadas[figurinha - 1 ] = QuantidadeFigurinhasAlbum[figurinha - 1];
+                Console.WriteLine($"Figurinha {figurinha} colada no album!");
+            }
+        }
+        public void MostrarAlbum()
+        {
+            Console.WriteLine($"\n\nO Album Contém {QuantidadeFigurinhasAlbum.Length} figurinhas!\n" +
+                $"Os locais [0] indicam figurinhas que ainda não foram coladas\n");
+            int j = 1;
+            foreach(int i in FigurinhasColadas)
+            {
+                if (j == 10)
+                {
+                    Console.Write($"[{i}]\t\n");
+                    j = 1;
+                }
+                else
+                {
+                    Console.Write($"[{i}]\t");
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ Exercício 14/Exercício 14/Program.cs
@@ -0,0 +1,21 @@
+﻿//Faça uma classe Album (de figurinhas) que tem como propriedades a quantidade de figurinhas e um array de
+//figurinhasColadas que recebe os números das figurinhas coladas. Garanta que o array de figurinhasColadas apenas 
+//pode crescer. Tenha também um método colarFigurinha que permita colar figurinhas que existam no álbum e que não
+//foram coladas ainda.
+
+using Exercício_14;
+using System.Security.Cryptography;
+
+AlbumDeFigurinhas a1 = new AlbumDeFigurinhas(50);
+a1.ColarFigurinhas(4);
+a1.ColarFigurinhas(30);
+a1.ColarFigurinhas(71);
+a1.ColarFigurinhas(1);
+a1.ColarFigurinhas(8);
+a1.ColarFigurinhas(30);
+a1.ColarFigurinhas(67);
+a1.ColarFigurinhas(50);
+a1.ColarFigurinhas(24);
+a1.ColarFigurinhas(25);
+
+a1.MostrarAlbum();
