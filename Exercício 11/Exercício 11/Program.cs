8dae35bc42ccb60695593142c0d1c8f399200d72
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 11:54:22 -0300

    Inserindo exercício 11

---
+++ Exercício 11/Exercício 11/Program.cs
@@ -0,0 +1,18 @@
+﻿//Crie uma classe Restaurante com as propriedades cidade, delivery (verdadeiro ou falso), pedidosPendentes e 
+//tempo de entrega estimado (calculado com base nos pedidos pendentes, sendo 10 + 2 * pedidosPendentes em minutos).
+//Os restaurantes devem ser capazes de realizar entregas desde que o cliente esteja localizado na mesma cidade e,
+//para isso, precisamos de um método entregaPedido que devolve como resultado se é possível realizar a entrega 
+//para um determinado cliente ou não e, em caso positivo, o tempo estimado de entrega.
+
+using Exercício_11;
+
+Restaurante LancheDoGordo = new Restaurante("Indaiatuba", true);
+LancheDoGordo.Entrega();
+LancheDoGordo.Entrega();
+LancheDoGordo.Entrega();
+LancheDoGordo.Entrega();
+
+Console.WriteLine("Pedidos pendentes: " + LancheDoGordo.PedidosPendentes);
+
+Restaurante LancheDoGordo2 = new Restaurante("Indaiatuba", false);
+LancheDoGordo2.Entrega();
---
+++ Exercício 11/Exercício 11/Restaurante.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_11
+{
+    internal class Restaurante
+    {
+        private string Cidade { get; }
+        private bool Delivery { get; }
+        public int PedidosPendentes { get; set; }
+        private int TempoDeEntregaEstimado { get; set; }
+
+        public Restaurante(string cidade, bool delivery)
+        {
+            Cidade = cidade;
+            Delivery = delivery;
+            PedidosPendentes = 0;
+            TempoDeEntregaEstimado = 0;
+        }
+        public void Entrega()
+        {
+            if (Delivery)
+            {
+                Console.Write($"Olá! Nosso restaurante fica localizado em {Cidade}. Qual a sua cidade?  : ");
+                string cidade = Console.ReadLine();
+                if (cidade.ToUpper() == Cidade.ToUpper())
+                {
+                    TempoDeEntregaEstimado = (10 + 2*PedidosPendentes);
+                    Console.WriteLine($"Perfeito! Seu pedido está em andamento! " +
+                        $"o tempo estimado de entrega é {TempoDeEntregaEstimado} minutos");
+                    PedidosPendentes++;
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente não realizamos entregas em outra cidade!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente nosso restaurante não realiza delivery," +
+                    " venha nos fazer uma visita para experimentar nossos pratos ! estamos localizados" +
+                    " na cidade de " + Cidade);
+            }
+        }
+    }
+}
