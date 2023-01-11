4de188015055070c55d8e7598751166bfeac1503
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-11 11:25:42 -0300

    inserindo exercício 10

---
+++ Exercício 10/Exercício 10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Deseja - se montar uma classe Sorteio que tem o número de pessoas inscritas e um método sorteiaPremio
+//que sorteia um número que corresponderá ao número de inscrição do participante e declara o vencedor.
+
+using Exercício_10;
+
+int classificado = Sorteio.SorteiaPremio(200);
---
+++ Exercício 10/Exercício 10/Sorteio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_10
+{
+    static class Sorteio
+    {
+        public static int SorteiaPremio(int participantes)
+        {
+            Random random = new Random();
+            int n;
+            n = random.Next(1,participantes+1);
+            Console.WriteLine($"O participante PREMIADO foi o de inscrição n° {n}");
+            return n;
+        }
+    }
+}
